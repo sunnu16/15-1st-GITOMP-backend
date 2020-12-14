61e64cdf8efd0ab348845b164ccca6bcd9609eac
seungjae_kim <kimsj950902@gmail>
2020-12-14 19:35:29 +0900

    initial settings

--- gitomp/settings.py
+++ gitomp/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = SECRET_KEY
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
