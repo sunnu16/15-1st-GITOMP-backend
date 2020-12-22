e47f1ccf53ea6de6ecb4593229d6dde2259e51de
seungjae_kim <kimsj950902@gmail>
2020-12-22 16:23:33 +0900

    유저 회원가입 메소드 가독성 및 유지보수 개선

--- users/utils.py
+++ users/utils.py
@@ -2,16 +2,22 @@ import jwt
 import json
 import re
 
-from users.models import User
-from my_settings  import SECRET_KEY,JWT_ALGORITHM
-from django.http  import JsonResponse
+from users.models     import User
+from django.db.models import Q
+from my_settings      import SECRET_KEY,JWT_ALGORITHM
+from django.http      import JsonResponse
 
-PW_REGEX       = '^[A-Za-z0-9~`!@#$%\^&*()-+=]{8,256}$'
-EMAIL_REGEX    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-NICKNAME_REGEX = '^[A-Za-z0-9ㄱ-힣~`!@#$%\^&*()-+=]{3,16}$'
+def validate_email(text):
+    EMAIL_REGEX    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.compile(EMAIL_REGEX).match(text)
 
-def is_valid(text, regex):
-    return re.compile(regex).match(text)
+def validate_password(text):
+    PW_REGEX       = '^[A-Za-z0-9~`!@#$%\^&*()-+=]{8,256}$' 
+    return re.compile(PW_REGEX).match(text)
+
+def validate_nickname(text):
+    NICKNAME_REGEX = '^[A-Za-z0-9ㄱ-힣~`!@#$%\^&*()-+=]{3,16}$'
+    return re.compile(NICKNAME_REGEX).match(text)
 
 def LoginConfirm(original_function):
     def wrapper(self, request, *args, **kwargs):
@@ -34,3 +40,34 @@ def LoginConfirm(original_function):
         except Users.DoesNotExist:
             return JsonResponse({'message':'INVALID_USER'}, status=401)
     return wrapper
+
+def validate_signup(original_function):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+            request.email = data['email']
+            request.password = data['password']
+            request.nickname = data['nickname']
+            
+            if not validate_email(request.email):
+                return JsonResponse({"MESSAGE":"INVALID_EMAIL"},status=400)
+            
+            if not validate_password(request.password):
+                return JsonResponse({"MESSAGE":"INVALID_PASSWORD"},status=400)
+            
+            if not validate_nickname(request.nickname):
+                return JsonResponse({"MESSAGE":"INVALID_NICKNAME"},status=400)
+            
+            if User.objects.filter(Q(nickname=request.nickname)|Q(email=request.email)).exists():
+                return JsonResponse({"MESSAGE":"INVALID_USER"},status=400)
+
+            return original_function(self, request, *args, **kwargs)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({"MESSAGE","INVALID_DATA"},status=400)
+    
+    return wrapper
+
+
